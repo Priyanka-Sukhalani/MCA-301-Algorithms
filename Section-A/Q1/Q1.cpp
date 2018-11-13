799bcf25bb88e401a3fd77c2b02603b85e96b9f2
Priyanka-Sukhalani <39946934+Priyanka-Sukhalani@users.noreply.github.com>
2018-11-13 23:06:11 +0530

    Updated Documentation

--- Section-A/Q1/Q1.cpp
+++ Section-A/Q1/Q1.cpp
@@ -21,10 +21,11 @@ void print_optimal_paranthesis(int i,int j) {
         cout<<"A"<<i+1;
     }
     else {
-        cout<<"(";
+        // Recursion to print Paranthesis
+        cout<<"(";                                  // Left Paranthesis
         print_optimal_paranthesis(i,s[i][j]);
         print_optimal_paranthesis(s[i][j]+1,j);
-        cout<<")";
+        cout<<")";                                  // Right Paranthesis
     }
 }
 
@@ -37,7 +38,7 @@ int no_of_multiplication(vector<int> dim) {
     s = new int *[n];
     
     int temp = n;
-    for(int i=0;i<n;i++) {
+    for(int i=0;i<n;i++) {                      // to optimise space complexity allocate only upper diagonal matrix
         mat[i] = new int[temp];
         s[i] = new int[temp];
         temp--;
@@ -57,9 +58,12 @@ int no_of_multiplication(vector<int> dim) {
             mat[i][j] = 9999999;
             
             for ( int k = i; k < j; k++ ) {
+                // Approach: using Dynamic Programming Properties
+                //           1. Optimal Substructure
+                //           2. Overlapping Subproblems
                 int q = mat[i][k] + mat[k+1][j] + dim[i]*dim[k+1]*dim[j+1];
                 
-                if ( q < mat[i][j] ) {
+                if ( q < mat[i][j] ) {          // minimum cost is selected
                     mat[i][j] = q;
                     s[i][j] = k;
                 }
@@ -70,7 +74,7 @@ int no_of_multiplication(vector<int> dim) {
     
 
 
-    return mat[0][n-1];
+    return mat[0][n-1];                         // minimum cost to multiply all the n matrices
 
     
 }
@@ -106,7 +110,7 @@ int main() {
         cout<<"\n No of Columns : ";
         cin>>col;
 
-        if ( row != dim[dim.size()-1] ) {
+        if ( row != dim[dim.size()-1] ) {                   // checking whether matrices are compatible or not.
             cout<<" Matrices are not compatible.";
             cout<<"\n Do You want to enter valid matrices : ";
             cin>>ans;
